ca931f4acb21a2c47b125a4a2ea4350f741d1a28
Dhirendra Kathayat <107436829+Dhiren1211@users.noreply.github.com>
2024-10-06 10:56:48 +0545

    Add files via upload

---
+++ ProjectTask/Admin/DatabaseManagement.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace ProjectTask.Admin
+{
+    partial class DatabaseManagement
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // DatabaseManagement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1536, 789);
+            this.Name = "DatabaseManagement";
+            this.Text = "DatabaseManagement";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
